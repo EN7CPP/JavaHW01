da4a1ed41cb104f78461c6c7b7709b4a2f1ac96e
EN7CPP <nouroyan@mail.ru>
2019-01-30 23:02:24 +0400

    Add files via upload

---
+++ ApplesToPears.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String Input_1=scan.nextLine();
+		System.out.println(Input_1.replaceFirst("apple","pear"));
+		String Input_2=scan.nextLine();
+		System.out.println(Input_2.replaceFirst("apple","pear"));
+		String Input_3=scan.nextLine();
+		System.out.println(Input_3.replaceFirst("apple","pear"));
+		
+		
+	}
+}
---
+++ CircAreaCalc.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    double pi=3.14;
+		Scanner scan=new Scanner(System.in);
+		double radius=scan.nextDouble();
+		System.out.println("The circumference is: " +(2*pi*radius)+"\n"+"The area is: " +(pi*radius*radius) );
+		
+	}
+}
